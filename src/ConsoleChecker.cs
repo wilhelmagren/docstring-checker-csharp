3ef0b7437276be6a92446fa477285bcd89379900
wilhelmagren <wilhelmagren98@gmail.com>
2024-05-16 23:38:19 +0200

    [feat] start of c# program

---
+++ src/ConsoleChecker.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DocstringChecker {
+    class DocstringChecker {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
