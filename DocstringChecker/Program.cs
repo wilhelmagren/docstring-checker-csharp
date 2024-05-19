923966c76ab14838a77682de3e7db867af962dce
wilhelmagren <wilhelmagren98@gmail.com>
2024-05-19 08:43:47 +0200

    fuck this ecosystem, microsoft, please do something better...

---
+++ DocstringChecker/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length != 1) {
+            Console.Error.Write("You need to provide exactly one argument.");
+            Environment.Exit(1);
+        }
+
+        string root = args[0];
+        Console.Write("you want to search from " + root + " ?");
+
+    }
+}
--- src/ConsoleChecker.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DocstringChecker {
-    class DocstringChecker {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
